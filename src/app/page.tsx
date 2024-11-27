c8915cce44e0d17e5cb31967ca9a47a26072b746
Dano <danocolombo@gmail.com>
2024-11-26 20:26:46 -0500

    Home page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
         <div className='bg-black bg-home-img bg-cover bg-center'>
             <main className='flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh'>
                 <div className='flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl'>
-                    <h1>
+                    <h1 className='text-3xl font-bold'>
                         Dano&apos;s Computer
                         <br />
                         Repair Shop
