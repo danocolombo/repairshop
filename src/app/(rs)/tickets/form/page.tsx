1aa3a71475e409f06cb37ce81b62b8401a30b08d
Dano <danocolombo@gmail.com>
2024-11-29 15:54:36 -0500

    customers and tickets form available

--- src/app/(rs)/tickets/form/page.tsx
+++ src/app/(rs)/tickets/form/page.tsx
@@ -2,7 +2,7 @@ import { getCustomer } from '@/lib/queries/getCustomer';
 import { getTicket } from '@/lib/queries/getTicket';
 import { BackButton } from '@/components/BackButton';
 // import * as Sentry from '@sentry/nextjs';
-// import TicketForm from '@/app/(rs)/tickets/form/TicketForm';
+import TicketForm from '@/app/(rs)/tickets/form/TicketForm';
 
 export default async function TicketFormPage({
     searchParams,
@@ -51,7 +51,7 @@ export default async function TicketFormPage({
 
             // return ticket form
             console.log(customer);
-            // return <TicketForm customer={customer} />;
+            return <TicketForm customer={customer} />;
         }
 
         // Edit ticket form
@@ -74,7 +74,7 @@ export default async function TicketFormPage({
             // return ticket form
             console.log('ticket: ', ticket);
             console.log('customer: ', customer);
-            // return <TicketForm customer={customer} ticket={ticket} />;
+            return <TicketForm customer={customer} ticket={ticket} />;
         }
     } catch (e) {
         if (e instanceof Error) {
